b204e00cf9488d31ba6c56a498d7c6e8e6ee727e
chenj5618 <76765168@qq.com>
2019-04-26 08:11:46 +0800

    1

--- src/main/java/fuck996/tree/Tree.java
+++ src/main/java/fuck996/tree/Tree.java
@@ -4,6 +4,18 @@ import java.util.List;
 
 public class Tree {
 
+	/**
+	 * 树的遍历可分为
+	 * 
+	 * 前序遍历：先访问根节点，再访问左子树，最后访问右子树
+	 * 
+	 * 中序遍历：先访问左子树，再访问根节点，最后访问右子树
+	 * 
+	 * 后序遍历：先访问左子树，再访问右子树，最后访问根节点
+	 * 
+	 * 三种遍历方式可以用递归来实现，可以可以用非递归方式来遍历
+	 */
+
 	private transient TreeNode<Object> root;
 
 	public TreeNode<Object> getRoot() {
@@ -20,7 +32,7 @@ public class Tree {
 		TreeNode<Object> root = tree.getRoot();
 		traversal(root);
 	}
-
+	//递归方式的前序遍历
 	private static void traversal(TreeNode<Object> node) {
 
 		if (node == null) {
